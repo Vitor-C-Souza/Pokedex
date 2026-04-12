b98dbe5e5dbde0b0899b7146048fd2af2e034e30
Vítor Cavalcante Souza <51930276+Vitor-C-Souza@users.noreply.github.com>
2026-04-12 01:02:59 -0300

    Add WorkManager dependency and update library versions
    - Add `androidx.work:work-runtime-ktx` dependency to `app/build.gradle.kts` and `libs.versions.toml`.
    - Update Koin to version 4.2.1.
    - Update KSP plugin to version 2.1.0-1.0.29.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(libs.androidx.work.runtime.ktx)
 }
